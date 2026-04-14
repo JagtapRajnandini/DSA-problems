ecfd7b4f8a7970ec58eb002d8da63f477845ac65
Rajnandini Jagtap <jagtaprajnandini653@gmail.com>
2026-04-14 22:11:30 +0530

    Solved:Linear search using recursion

--- Recursion_Array_Questions.java
+++ Recursion_Array_Questions.java
@@ -4,21 +4,30 @@ public class Recursion_Array_Questions
 public static void main(String[] args)
 {
     int[] arr={1,2,3,5,689,7};
-  check_if_Array_is_sorted(arr, 0);
+  //check_if_Array_is_sorted(arr, 0);
+  System.out.println(linear_search(arr, 1, 0));
 }
 
-static void check_if_Array_is_sorted(int[] arr,int i)
+static boolean check_if_Array_is_sorted(int[] arr,int i)
 {
-  if(i==arr.length-1)
+  if(arr.length <= 1|| i==arr.length-1)
   {
-    System.out.println("Array is sorted !!");
-    return;
+    return true;
   }
   if(arr[i]>arr[i+1])
   {
-    System.out.println("Array is not sorted !!");
-    return;
+    return false;
   }
-  check_if_Array_is_sorted(arr, i+1);
+  return check_if_Array_is_sorted(arr, i+1);
+}
+
+static boolean linear_search(int[] arr,int target,int i)
+{
+ if(i==arr.length)
+ {
+  return false;
+ }
+
+ return arr[i]==target || linear_search(arr,target,i+1);
 }
 }
