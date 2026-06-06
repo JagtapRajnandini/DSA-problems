7457540d4bba4b3a2513dcb29b8aecc457233270
Rajnandini Jagtap <jagtaprajnandini653@gmail.com>
2026-06-06 11:47:50 +0530

    Recursion_Array_Questions

---
+++ Array/Recursion_Array_Questions.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+public class Recursion_Array_Questions
+{
+public static void main(String[] args)
+{
+    int[] arr={1,2,3,5,689,7,2};
+  //check_if_Array_is_sorted(arr, 0);
+  //System.out.println(linear_search(arr, 1, 0));
+  ArrayList<Integer> list=new ArrayList<>();
+  System.out.println(findAllIndices(arr, 2, 0, list));
+  System.out.println(return_list_without_passing_it_as_argument(arr, 2, 0));
+}
+
+static boolean check_if_Array_is_sorted(int[] arr,int i)
+{
+  if(arr.length <= 1|| i==arr.length-1)
+  {
+    return true;
+  }
+  if(arr[i]>arr[i+1])
+  {
+    return false;
+  }
+  return check_if_Array_is_sorted(arr, i+1);
+}
+
+static boolean linear_search(int[] arr,int target,int i)
+{
+ if(i==arr.length)
+ {
+  return false;
+ }
+//either one of the statements should for finding the target is not the current one try for next 
+ return arr[i]==target || linear_search(arr,target,i+1);
+}
+ 
+static ArrayList<Integer> findAllIndices(int[] arr, int target, int i, ArrayList<Integer> list) {
+    if (i == arr.length) {
+        return list;
+    }
+    if (arr[i] == target) {
+        list.add(i);
+    }
+    return findAllIndices(arr, target, i + 1, list);
+}
+
+static ArrayList<Integer> return_list_without_passing_it_as_argument(int[] arr,int target, int i)
+{
+  ArrayList<Integer> list=new ArrayList<>();
+  if(i==arr.length)
+  {
+    return list;
+  }
+  if(arr[i]==target)
+  {
+    list.add(i);
+  }
+  ArrayList<Integer> ansFromBelowCalls=return_list_without_passing_it_as_argument(arr, target, i+1);
+  list.addAll(ansFromBelowCalls);
+  return list;
+}
+}
